c088c1bce2b537b55764675fc34e487fdca95016
EvaRoets <eva_roets@hotmail.com>
2021-06-17 13:55:09 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+//TODO phase 1
+// three rock-paper-scissors choice buttons (emoji's)
+// one button to have the computer pick one randomly
+// Register button click
+// Announce the winner through html message
+// Possible outcomes: draw (when both chose the same thing) or player won/lost (if else if else? - switch)
