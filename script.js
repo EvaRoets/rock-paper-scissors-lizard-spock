bc3aea21505517f429c15a6a224c095ee4021444
EvaRoets <eva_roets@hotmail.com>
2021-06-18 09:58:06 +0200

    Add basic structure and to dos

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 //TODO phase 1
-// three rock-paper-scissors choice buttons (emoji's)
-// one button to have the computer pick one randomly
-// Register button click
-// Announce the winner through html message
+// register one of three rock-paper-scissors buttons OR
+// register click random computer choice
+// if else if else:Announce the winner through html message (no alert) = scoreboard?
 // Possible outcomes: draw (when both chose the same thing) or player won/lost (if else if else? - switch)
+
+// TODO phase 2
+// different end message styling depending on the outcome (remove/add classes in js) -- phase2
+// play again button after game ended
+// styling (css)
+// clean code (html, css, js) - delete redundant code or out of date comments
+// use functions to simplify code?
+// make code more readable to other devs
+
+// TODO nice to have
+// extra  player options: lizard & spock
+// scoreboard keeping track after multiple games
+// highlight chosen button
+// more player options (ex. high points, low chance)
+// nog if statements (=> switch/case)
+// make player win 75-100% of  games?
