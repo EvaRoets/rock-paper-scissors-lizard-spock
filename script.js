c054c993254df3e27c363146ddfb553976ae2019
EvaRoets <eva_roets@hotmail.com>
2021-06-21 11:43:33 +0200

    Add outcomes and play btn

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 //TODO phase 1
 // register one of three rock-paper-scissors buttons OR
+
+
+
+
 // register click random computer choice
 // if else if else:Announce the winner through html message (no alert) = scoreboard?
 // Possible outcomes: draw (when both chose the same thing) or player won/lost (if else if else? - switch)
 
+
+// It's a draw! Play again.
+
+//ROCK
+//You win, rock crushes scissors!
+//You win, rock crushes lizard!
+//You loose, paper covers rock!
+//You loose, Spock vaporizes rock!
+
+//PAPER
+//You win, paper disproves Spock!
+//You win, paper covers rock!
+//You loose, scissors cuts paper!
+//You loose, lizard eats paper!
+
+//SCISSORS
+//You win, scissors cuts paper!
+//You win, scissors decapitate lizard!
+//You loose, rock crushes scissors!
+//You loose, Spock smashes scissors!
+
+//SPOCK
+//You win, Spock smashes scissors!
+//You win, Spock vaporizes rock!
+//You loose, lizard poisons spock!
+//You loose, paper disproves Spock!
+
+//LIZARD
+//You win, lizard poisons spock!
+//You win, lizard eats paper!
+//You loose, scissors decapitate lizard!
+//You loose, rock crushes lizard!
+
+
 // TODO phase 2
 // different end message styling depending on the outcome (remove/add classes in js) -- phase2
-// play again button after game ended
-// styling (css)
+// play again button after game ended OK
+// styling (css) OK
 // clean code (html, css, js) - delete redundant code or out of date comments
 // use functions to simplify code?
 // make code more readable to other devs
 
 // TODO nice to have
-// extra  player options: lizard & spock
-// scoreboard keeping track after multiple games
-// highlight chosen button
+// extra  player options: lizard & spock: OK
+// scoreboard keeping track after multiple games: OK
+// highlight chosen button: OK
 // more player options (ex. high points, low chance)
 // nog if statements (=> switch/case)
 // make player win 75-100% of  games?
