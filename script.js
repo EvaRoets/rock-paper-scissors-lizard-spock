a2688bb70fbd5f52f26e906d927e549e2f1f6c04
EvaRoets <eva_roets@hotmail.com>
2021-06-24 15:13:25 +0200

    Add event clicks and if loops

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
-//options
+//player can push button
 const rock = document.getElementById("rock");
 const paper = document.getElementById("paper");
 const scissors = document.getElementById("scissors");
 const lizard = document.getElementById("lizard");
-const spock =document.getElementById("spock");
-const pcVsPc =document.getElementById("pcVsPc");
+const Spock = document.getElementById("spock");
 
-// register clicks - declare function
-// player's choice
-function getPlayerChoice () {
+//computer makes random choice
+let play = document.getElementById("play");
+
+const result = result;
+
+// when player pushes play
+play.addEventListener("click", function () {
+    const playOptions = ["rock", "paper", "scissors", "spock", "lizard"]; // 4 indices, 5 length
+    let randomChoice = playOptions[Math.floor(Math.random() * playOptions.length)]
+    //console.log(randomChoice)
+})
+
+// when player pushes button
+function getPlayerChoice() {
     rock.addEventListener("click", function () {
         clickCapture("rock");
         paper.style.display = "none";
@@ -17,7 +27,6 @@ function getPlayerChoice () {
         lizard.style.display = "none";
         pcVsPc.style.display = "none";
         //console.log("You clicked rock");
-
     });
     paper.addEventListener("click", function () {
         clickCapture("paper");
@@ -55,134 +64,142 @@ function getPlayerChoice () {
         pcVsPc.style.display = "none";
         //console.log("You clicked spock");
     });
-    //TODO change displayed name pcVsPc to "Computer fights computer" + delay of result?
-    pcVsPc.addEventListener("click", function () {
-        clickCapture("pcVsPc");
-        rock.style.display = "none";
-        paper.style.display = "none";
-        scissors.style.display = "none";
-        spock.style.display = "none";
-        lizard.style.display = "none";
-        console.log("You let the computer fight you battle");
-    });
+    //change displayed name pcVsPc to "Computer fights computer" + delay of result?
+    // pcVsPc.addEventListener("click", function () {
+    //     clickCapture("pcVsPc");
+    //     rock.style.display = "none";
+    //     paper.style.display = "none";
+    //     scissors.style.display = "none";
+    //     spock.style.display = "none";
+    //     lizard.style.display = "none";
+    //     console.log("You let the computer fight you battle");
+    // });
 }
-//computer's choice
-function getComputerChoice() {
-    const playOptions = ["rock", "paper", "scissors", "spock", "lizard"]; // 4 indices, 5 length
-    const randomChoice = [Math.floor(Math.random() * playOptions.length)];
-    return playOptions[randomChoice];
+
+//possible outcomes
+if (playerChoice === computerChoice) {
+result.style.display = "inline";
+result.innerHTML = "It's a draw! Play again to collect more points.";
 }
-//store clicks
-function clickCapture (playerChoice) {
-    const computerChoice = getComputerChoice();
-    console.log("You choose " + playerChoice);
-    console.log("The computer choose " + computerChoice);
+//YOU WIN
+//ROCK
+else if (playerChoice === rock && computerChoice === scissors) {
+result.innerHTML = `You win, rock crushes scissors!`; //replace with string literals
+playerScore++;
 }
-//call function
-getPlayerChoice ();
+else if (playerChoice === rock && computerChoice === lizard) {
+result.innerHTML = `You win, rock crushes lizard!`; //replace with string literals
+    playerScore++;
 
-//compare scores - declare function
-function getResult() {
-    const result = document.getElementById("result");
-    // if -else OR switch?
-    if (playerChoice === computerChoice) {
-        result.style.display = "inline";
-        result.innerHTML = "It's a draw! Play again to collect more points.";
-    }
-    //YOU WIN
-    else if ()
-        //ROCK
-        //You win, rock crushes scissors!
-        //You win, rock crushes lizard!
-        //PAPER
-        //You win, paper disproves Spock!
-        //You win, paper covers rock!
-
-        //SCISSORS
-        //You win, scissors cuts paper!
-        //You win, scissors decapitate lizard!
-
-        //SPOCK
-        //You win, Spock smashes scissors!
-        //You win, Spock vaporizes rock!
-
-        //LIZARD
-        //You win, lizard poisons spock!
-        //You win, lizard eats paper!
-
-    //YOU LOOSE/Computer wins
-    else if ()
-        //ROCK
-        //You loose, paper covers rock!
-        //You loose, Spock vaporizes rock!
-
-        //PAPER
-        //You loose, scissors cuts paper!
-        //You loose, lizard eats paper!
-
-        //SCISSORS
-        //You loose, rock crushes scissors!
-        //You loose, Spock smashes scissors!
-
-        //SPOCK
-        //You loose, lizard poisons spock!
-        //You loose, paper disproves Spock!
-
-        //LIZARD
-        //You loose, scissors decapitate lizard!
-        //You loose, rock crushes lizard!
-
-        }
-
-//call function
-getResult();
-
-
-
-/*
-//letsPlay();
-//function letsPlay(){ //
-
-
-
-    else if (playerChoice === 0 && computerChoice === 2) {
-        result.style.display="inline";
-        result.innerHTML = `You win`;
-        playerScore++;
-    }
-
-
-    else if (playerChoice === 2 && computerChoice === 0) {
-        result.style.display="inline";
-        document.getElementById("result").innerHTML = `Computer wins`;
-        computerScore++;
-    }
-    else if (playerChoice < randomIcon) {
-        result.style.display="inline";
-        document.getElementById("result").innerHTML = `Computer wins`;
-        computerScore++;
-    }
-    else {
-        result.style.display="inline";
-        document.getElementById("result").innerHTML = `You win`
-        userScore++;
-    }
-    document.getElementById("playAgain").style.display="inline";
+//PAPER
 }
+else if (playerChoice === paper && computerChoice === Spock) {
+result.innerHTML = `You win, paper disproves Spock!`; //replace with string literals
+    playerScore++;
+}
+else if (playerChoice === paper && computerChoice === rock) {
+result.innerHTML = `You win, paper covers rock!`; //replace with string literals
+    playerScore++;
+
+//SCISSORS
+}
+else if (playerChoice === scissors && computerChoice === paper) {
+result.innerHTML = `You win, scissors cuts paper!`; //replace with string literals
+    playerScore++;
+}
+else if (playerChoice === scissors && computerChoice === lizard) {
+result.innerHTML = `You win, scissors decapitate lizard!`; //replace with string literals
+    playerScore++;
+
+//SPOCK
+}
+else if (playerChoice === Spock && computerChoice === scissors) {
+result.innerHTML = `You win, Spock smashes scissors!`; //replace with string literals
+    playerScore++;
+}
+else if (playerChoice === Spock && computerChoice === rock) {
+result.innerHTML = `You win, Spock vaporizes rock!`; //replace with string literals
+    playerScore++;
+
+//LIZARD
+}
+else if (playerChoice === lizard && computerChoice === Spock) {
+    result.innerHTML = `You win, lizard poisons Spock!`; //replace with string literals
+    playerScore++;
+}
+else if (playerChoice === lizard && computerChoice === paper) {
+        result.innerHTML = `You win, lizard eats paper!`; //replace with string literals
+    playerScore++;
+}
+//YOU LOOSE
+//ROCK
+else if (playerChoice === rock && computerChoice === paper) {
+    result.innerHTML = `You loose, paper covers rock!`; //replace with string literals
+    computerScore++;
+}
+else if (playerChoice === rock && computerChoice === Spock) {
+    result.innerHTML = `You loose, Spock vaporizes rock!`; //replace with string literals
+    computerScore++;
+//PAPER
+}
+else if (playerChoice === paper && computerChoice === scissors) {
+    result.innerHTML = `You loose, scissors cuts paper!`; //replace with string literals
+    computerScore++;
+}
+else if (playerChoice === paper && computerChoice === lizard) {
+    result.innerHTML = `You loose, lizard eats paper!`; //replace with string literals
+    computerScore++;
+// SCISSORS
+}
+else if (playerChoice === scissors && computerChoice === rock) {
+    result.innerHTML = `You loose, rock crushes scissors!`; //replace with string literals
+    computerScore++;
+}
+else if (playerChoice === scissors && computerChoice === Spock) {
+    result.innerHTML = `You loose, Spock smashes scissors!`; //replace with string literals
+    computerScore++;
+// SPOCK
+}
+else if (playerChoice === Spock && computerChoice === lizard) {
+    result.innerHTML = `You loose, lizard poisons Spock!`; //replace with string literals
+    computerScore++;
+}
+else if (playerChoice === Spock && computerChoice === paper) {
+    result.innerHTML = `You loose, paper disproves Spock!`; //replace with string literals
+    computerScore++;
+// LIZARD
+}
+else if (playerChoice === lizard && computerChoice === scissors) {
+    result.innerHTML = `You loose, scissors decapitate lizard!`; //replace with string literals
+    computerScore++;
+}
+else if (playerChoice === lizard && computerChoice === rock) {
+    result.innerHTML = `You loose, rock crushes lizard!`; //replace with string literals
+    computerScore++;
 }
 
 
 
 
 
+
+
+//     document.getElementById("playAgain").style.display="inline";
+// }
+// }
+//
+//
+//
+//
+//
 // TODO phase 2
 // different end message styling depending on the outcome (remove/add classes in js) -- phase2
-const playerScore = 0; //TODO update
-const computerScore = 0;//TODO update
-const displayPlayerScore = document.getElementById("playerscore");
-const displayComputerScore = document.getElementById("computerscore");
-
-
+// const playerScore = 0; //TODO update
+// const computerScore = 0;//TODO update
+// const displayPlayerScore = document.getElementById("playerscore");
+// const displayComputerScore = document.getElementById("computerscore");
+//
+//
 // play again button after game ended OK ---- > make it appear only after game
 // clean code (html, css, js) - delete redundant code or out of date comments
 // use functions to simplify code?
@@ -194,6 +211,3 @@ const displayComputerScore = document.getElementById("computerscore");
 // nog if statements (=> switch/case)
 // make player win 75-100% of  games?
 
-
-
- */
